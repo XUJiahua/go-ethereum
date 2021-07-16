0a8af7d1c11d25b03a55cda571e1d795255cce5f
jiahua <jiahua.xu@outlook.com>
2021-07-16 23:16:34 +0800

    cr: private network

--- private/geth.sh
+++ private/geth.sh
@@ -13,7 +13,7 @@ bootnodes="enode://fe0eaa382868b61a5712dcb8690b0334a446f08425288fe5c20dc4dd92b70
 port="3030$1"
 httpport="810$1"
 log="$dir/0$1.log"
-geth --networkid=1000 --datadir="$datadir" --verbosity 6 \
-  --ipcdisable --port $port --http.port $httpport \
+geth --datadir="$datadir" --verbosity 6 \
+  --ipcdisable --port $port --http --http.port $httpport \
   --bootnodes "$bootnodes" \
   console 2>> $log
